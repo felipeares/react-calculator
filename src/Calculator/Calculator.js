24f8c06a7205befad3bc1913910cf1bcca922819
Felipe Ares <felipeares@gmail.com>
2019-12-11 17:37:06 -0300

    <Calculator /> added a Display

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import classes from "./Calculator.module.css";
+import Display from "./Display/Display";
 
 class Calculator extends Component {
   render() {
-    return <div class={classes.Calculator}>Calculator Component Works</div>;
+    return (
+      <div className={classes.Calculator}>
+        <Display />
+      </div>
+    );
   }
 }
 
--- src/Calculator/Calculator.test.js
+++ src/Calculator/Calculator.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Calculator from "./Calculator";
+import Display from "./Display/Display";
 
 describe("Calculator", () => {
   let wrapper;
@@ -11,5 +12,18 @@ describe("Calculator", () => {
 
   it("should have a main div", () => {
     expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should have a main class named Calculator", () => {
+    expect(
+      wrapper
+        .find("div")
+        .first()
+        .hasClass("Calculator")
+    ).toEqual(true);
+  });
+
+  it("should have a <Display />", () => {
+    expect(wrapper.find(Display)).toHaveLength(1);
   });
 });
