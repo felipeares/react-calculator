54894d03136ae7ac6c3cd390937b3f9589177e59
Felipe Ares <felipeares@gmail.com>
2019-12-11 18:43:54 -0300

    <Display /> updated snapshot with <p>

--- src/Calculator/Display/Display.js
+++ src/Calculator/Display/Display.js
@@ -3,7 +3,9 @@ import styles from "./Display.module.css";
 import PropTypes from "prop-types";
 
 const Display = props => (
-  <div className={styles.Display}>{props.displayValue}</div>
+  <div className={styles.Display}>
+    <p className="display-value">{props.displayValue}</p>
+  </div>
 );
 
 Display.propTypes = { displayValue: PropTypes.string.isRequired };
