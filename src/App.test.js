70c48b278271002d6d24422efe4484e318e93e6b
Felipe Ares <felipeares@gmail.com>
2019-12-11 17:02:40 -0300

    <App /> added a shallow snapshot unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 import App from "./App";
 import Calculator from "./Calculator/Calculator";
 
@@ -10,8 +11,16 @@ describe("App", () => {
     wrapper = shallow(<App />);
   });
 
-  it("renders without crashing", () => {
+  it("have not changed the UI", () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("should render a div", () => {
     expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should have a named App class", () => {
+    expect(wrapper.find("div").hasClass("App")).toEqual(true);
   });
 
   it("should render the Calculator component", () => {
