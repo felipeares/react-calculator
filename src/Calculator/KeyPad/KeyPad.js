395471d1a47e9cba9e2d9bbb6e45080606a262d5
Felipe Ares <felipeares@gmail.com>
2019-12-13 10:43:17 -0300

    <KeyPad /> added the props

--- src/Calculator/KeyPad/KeyPad.js
+++ src/Calculator/KeyPad/KeyPad.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./KeyPad.module.css";
+import PropTypes from "prop-types";
 
 const KeyPad = () => {
   return <div className={styles.KeyPad}></div>;
+};
+
+KeyPad.propTypes = {
+  numberPressed: PropTypes.func.isRequired,
+  dotPressed: PropTypes.func.isRequired,
+  operatorPressed: PropTypes.func.isRequired,
+  resetPressed: PropTypes.func.isRequired
 };
 
 export default KeyPad;
--- src/Calculator/KeyPad/KeyPad.test.js
+++ src/Calculator/KeyPad/KeyPad.test.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
+import PropTypes from "prop-types";
 import KeyPad from "./KeyPad";
 
 describe("KeyPad", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<KeyPad />);
+    wrapper = shallow(
+      <KeyPad
+        numberPressed={jest.fn()}
+        dotPressed={jest.fn()}
+        operatorPressed={jest.fn()}
+        resetPressed={jest.fn()}
+      />
+    );
   });
 
   it("should render a main div", () => {
@@ -20,5 +28,12 @@ describe("KeyPad", () => {
         .first()
         .hasClass("KeyPad")
     ).toEqual(true);
+  });
+
+  it("should check for all prop types correctly setup", () => {
+    expect(KeyPad.propTypes.numberPressed).toBe(PropTypes.func.isRequired);
+    expect(KeyPad.propTypes.dotPressed).toBe(PropTypes.func.isRequired);
+    expect(KeyPad.propTypes.operatorPressed).toBe(PropTypes.func.isRequired);
+    expect(KeyPad.propTypes.resetPressed).toBe(PropTypes.func.isRequired);
   });
 });
